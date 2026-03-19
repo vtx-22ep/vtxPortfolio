f0d0b0c427297c88582738be02d9a03245f66f28
vigneshthilak <vigneshthilagaraj00@gmail.com>
2026-03-19 22:40:24 +0530

    The Navbar have been added to the /components successfully

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar(){
+    return(
+        <>
+            <nav>
+                <div className="nav-links">
+                <a href="#">
+                    <h3>vtx-22ep.com</h3>
+                </a>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">PROJECT</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
